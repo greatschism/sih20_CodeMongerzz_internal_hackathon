bb217bdf0f0631c724f1e42116ba354f3cb98955
Ritesh Ranjan Nahak <nriteshranjan@gmail.com>
2020-02-06 15:43:32 +0530

    SIH APP ARK all uia nd rest api designed

---
+++ Ml_vision/app/src/main/java/com/example/ml_vision/RetrofitClientInstance.java
@@ -0,0 +1,22 @@
+package com.example.ml_vision;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface RetrofitClientInstance
+{
+    String BASE_URL = "http://192.168.43.211:8000" ;
+
+    @Multipart
+    @POST("/ocr")
+    Call<LabReport> upload(@Part MultipartBody.Part imageFile,
+                              @Part("description") RequestBody description
+                );
+
+}
